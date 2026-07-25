20babe692ea3292083b8462f9fadf0a7c3442b75
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-07-25 21:41:16 +0530

    Update Newspaper3D.tsx

--- components/Newspaper3D.tsx
+++ components/Newspaper3D.tsx
@@ -1,14 +1,30 @@
 "use client";
 
-import { Float } from "@react-three/drei";
+import { Float, RoundedBox } from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function Newspaper3D() {
+  const paper = useLoader(
+    THREE.TextureLoader,
+    "/textures/paper.jpg"
+  );
+
+  paper.wrapS = paper.wrapT = THREE.RepeatWrapping;
+  paper.repeat.set(1, 1);
+
   return (
-    <Float speed={2} rotationIntensity={0.2} floatIntensity={0.2}>
-      <mesh rotation={[-1.2, 0, 0]}>
-        <boxGeometry args={[3, 0.04, 4]} />
-        <meshStandardMaterial color="#f5f1e8" />
-      </mesh>
+    <Float speed={2} floatIntensity={0.15}>
+      <group position={[0, 0.05, 0]}>
+        <RoundedBox args={[2.6, 0.04, 3.6]} radius={0.02}>
+          <meshStandardMaterial map={paper} />
+        </RoundedBox>
+
+        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.03, 0]}>
+          <planeGeometry args={[2.4, 3.4]} />
+          <meshBasicMaterial color="#111" transparent opacity={0.05} />
+        </mesh>
+      </group>
     </Float>
   );
 }
