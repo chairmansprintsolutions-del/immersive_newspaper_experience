2ee1207ba8881c98447920b82006957cf8953f04
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-07-26 00:10:08 +0530

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,32 +1,10 @@
-"use client";
-
-import { OrbitControls } from "@react-three/drei";
-
 import Floor from "./Floor";
-import Table from "./Table";
-import NewspaperScene from "./NewspaperScene";
 
 export default function Experience() {
   return (
     <>
-      <ambientLight intensity={0.8} />
-      <directionalLight
-        castShadow
-        position={[5, 8, 5]}
-        intensity={2}
-      />
-
+      <ambientLight intensity={1} />
       <Floor />
-      <Table />
-
-      <group position={[0, 0.79, 0]}>
-        <NewspaperScene />
-      </group>
-
-      <OrbitControls
-        makeDefault
-        enablePan={false}
-      />
     </>
   );
 }
