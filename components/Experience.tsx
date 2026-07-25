7dd7e068211696b449b6c47fb9f6dd626b4cda52
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-07-25 23:38:14 +0530

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,13 +1,12 @@
 "use client";
 
+import Floor from "./Floor";
+
 export default function Experience() {
   return (
     <>
       <ambientLight intensity={1} />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshNormalMaterial />
-      </mesh>
+      <Floor />
     </>
   );
 }
