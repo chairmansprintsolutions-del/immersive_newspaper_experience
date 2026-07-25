3fe4f787a76a3c36d9470dbccaea221b40473b14
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-07-25 23:01:43 +0530

    Create Experience.tsx

---
+++ components/scene/Experience.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Environment, OrbitControls } from "@react-three/drei";
+
+import Floor from "./Floor";
+import Table from "./Table";
+import Lights from "./Lights";
+import CafeScene from "./CafeScene";
+import NewspaperScene from "../newspaper/NewspaperScene";
+
+export default function Experience() {
+  return (
+    <>
+      <color attach="background" args={["#f6f2eb"]} />
+
+      <Lights />
+
+      <Environment
+        files="/hdr/cafe.hdr"
+        background={false}
+      />
+
+      <Floor />
+
+      <Table />
+
+      <CafeScene />
+
+      <group position={[0, 0.79, 0]}>
+        <NewspaperScene />
+      </group>
+
+      <OrbitControls
+        enablePan={false}
+        minDistance={3}
+        maxDistance={8}
+        maxPolarAngle={Math.PI / 2.05}
+      />
+    </>
+  );
+}
