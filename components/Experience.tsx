eaca9223ab7188d990c7f6a9bf79745f2cad5f63
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-07-25 23:38:59 +0530

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Floor from "./Floor";
+import Table from "./Table";
 
 export default function Experience() {
   return (
     <>
       <ambientLight intensity={1} />
       <Floor />
+      <Table />
     </>
   );
 }
