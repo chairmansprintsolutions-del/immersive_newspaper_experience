f1f2425d13939b908152dc22336f39188c4a95ad
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-08-22 10:52:32 +0530

    Update Experience.tsx

--- components/scene/Experience.tsx
+++ components/scene/Experience.tsx
@@ -1,14 +1,35 @@
 "use client";
 
+import { Environment, OrbitControls } from "@react-three/drei";
+
+import Floor from "./Floor";
+import Table from "./Table";
+import Lights from "./Lights";
+import CafeScene from "./CafeScene";
+import NewspaperScene from "../newspaper/NewspaperScene";
+
 export default function Experience() {
   return (
     <>
-      <ambientLight intensity={1} />
+      <color attach="background" args={["#f6f2eb"]} />
 
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshNormalMaterial />
-      </mesh>
+      <Lights />
+
+      <Floor />
+      <Table />
+
+      <CafeScene />
+
+      <group position={[0, 0.79, 0]}>
+        <NewspaperScene />
+      </group>
+
+      <OrbitControls
+        enablePan={false}
+        minDistance={3}
+        maxDistance={8}
+        maxPolarAngle={Math.PI / 2.05}
+      />
     </>
   );
 }
