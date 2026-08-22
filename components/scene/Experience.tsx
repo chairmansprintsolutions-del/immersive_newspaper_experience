bd4383fc92cdef2b7484cf172f1b7386dc024e29
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-08-22 12:54:44 +0530

    Update Experience.tsx

--- components/scene/Experience.tsx
+++ components/scene/Experience.tsx
@@ -50,7 +50,7 @@ export default function Experience() {
       {/* Newspaper */}
       <group
         position={[0, 0.84, 0]}
-        scale={[0.65, 0.65, 0.65]}
+        scale={[0.7, 0.7, 0.7]}
       >
         <NewspaperScene />
       </group>
