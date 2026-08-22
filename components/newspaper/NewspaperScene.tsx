08e0e238554f983b6cf071f78ff6f58291e46642
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-08-22 11:06:07 +0530

    Update NewspaperScene.tsx

--- components/newspaper/NewspaperScene.tsx
+++ components/newspaper/NewspaperScene.tsx
@@ -1,23 +1,20 @@
 "use client";
 
 import { useState } from "react";
-
 import NewspaperModel from "./NewspaperModel";
-import OpenAnimation from "./OpenAnimation";
 import ReadingMode from "./ReadingMode";
 
 export default function NewspaperScene() {
   const [open, setOpen] = useState(false);
 
   return (
-    <group
-      onClick={() => setOpen(!open)}
-    >
-      <OpenAnimation open={open}>
-        <NewspaperModel open={open} />
-      </OpenAnimation>
+    <group>
+      <NewspaperModel
+        open={open}
+        onToggle={() => setOpen(!open)}
+      />
 
-      <ReadingMode open={open} />
+      {open && <ReadingMode />}
     </group>
   );
 }
