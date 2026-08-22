32d25bc8cc94cfb2982dcac8376662d762f3a7f8
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-08-22 12:43:08 +0530

    Update CafeScene.tsx

--- components/scene/CafeScene.tsx
+++ components/scene/CafeScene.tsx
@@ -1,35 +1,5 @@
-cat > components/scene/CafeScene.tsx <<'EOF'
 "use client";
 
-import Model from "../loaders/Model";
-
 export default function CafeScene() {
-  return (
-    <>
-      <Model
-        path="/model/room.glb"
-        position={[0, 0, 0]}
-        scale={1}
-      />
-
-      <Model
-        path="/model/coffee_cup.glb"
-        position={[1.6, 0.85, -0.8]}
-        scale={0.4}
-      />
-
-      <Model
-        path="/model/chair.glb"
-        position={[3, 0, 1.8]}
-        scale={1}
-      />
-
-      <Model
-        path="/model/plant.glb"
-        position={[-2.5, 0, -1.8]}
-        scale={0.8}
-      />
-    </>
-  );
+  return null;
 }
-EOF
