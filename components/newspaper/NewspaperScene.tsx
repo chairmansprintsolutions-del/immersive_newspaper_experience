8baf20375852cd5f8046eb4e063eb22cd3cb38f4
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-08-22 13:02:56 +0530

    Update NewspaperScene.tsx

--- components/newspaper/NewspaperScene.tsx
+++ components/newspaper/NewspaperScene.tsx
@@ -1,54 +1,18 @@
 "use client";
 
-import * as THREE from "three";
-
 export default function NewspaperScene() {
   return (
-    <group>
-      {/* Main newspaper body */}
-      <mesh
-        position={[0, 0.04, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-        castShadow
-        receiveShadow
-      >
-        <boxGeometry args={[2.8, 3.8, 0.06]} />
-
-        <meshStandardMaterial
-          color="#f3eee2"
-          roughness={0.95}
-          side={THREE.DoubleSide}
-        />
-      </mesh>
-
-      {/* Slightly smaller top sheet */}
-      <mesh
-        position={[0, 0.075, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-        receiveShadow
-      >
-        <planeGeometry args={[2.68, 3.68]} />
-
-        <meshStandardMaterial
-          color="#fffdf5"
-          roughness={0.95}
-          side={THREE.DoubleSide}
-        />
-      </mesh>
-
-      {/* Centre fold */}
-      <mesh
-        position={[0, 0.082, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-      >
-        <planeGeometry args={[0.025, 3.5]} />
-
-        <meshBasicMaterial
-          color="#cfc7b7"
-          transparent
-          opacity={0.6}
-        />
-      </mesh>
-    </group>
+    <mesh
+      position={[0, 0.05, 0]}
+      rotation={[0, 0, 0]}
+      castShadow
+      receiveShadow
+    >
+      <boxGeometry args={[2.8, 0.06, 3.8]} />
+      <meshStandardMaterial
+        color="#f4efe3"
+        roughness={0.95}
+      />
+    </mesh>
   );
 }
