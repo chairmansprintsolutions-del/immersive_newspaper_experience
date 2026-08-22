380125a3bd8e847c17180f382f20531bd5b302c2
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-08-22 13:05:07 +0530

    Update NewspaperScene.tsx

--- components/newspaper/NewspaperScene.tsx
+++ components/newspaper/NewspaperScene.tsx
@@ -3,12 +3,13 @@
 export default function NewspaperScene() {
   return (
     <mesh
-      position={[0, 0.05, 0]}
+      position={[0, 0.02, 0]}
       rotation={[0, 0, 0]}
       castShadow
       receiveShadow
     >
       <boxGeometry args={[2.8, 0.06, 3.8]} />
+
       <meshStandardMaterial
         color="#f4efe3"
         roughness={0.95}
