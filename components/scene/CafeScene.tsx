438a38b54ae352966c07dc60b3fd423eb4a7393b
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-08-22 13:07:39 +0530

    Update CafeScene.tsx

--- components/scene/CafeScene.tsx
+++ components/scene/CafeScene.tsx
@@ -1,160 +1,215 @@
 "use client";
 
-import * as THREE from "three";
-
 export default function CafeScene() {
   return (
     <group>
-      {/* Back wall */}
-      <mesh position={[0, 2.5, -4]}>
-        <boxGeometry args={[12, 5, 0.15]} />
+      {/* =========================
+          BACK WALL
+      ========================= */}
+      <mesh position={[0, 3, -4]}>
+        <boxGeometry args={[12, 6, 0.15]} />
         <meshStandardMaterial
-          color="#e8dfd2"
+          color="#d8cec0"
           roughness={0.9}
         />
       </mesh>
 
-      {/* Left wall */}
-      <mesh position={[-6, 2.5, 0]}>
-        <boxGeometry args={[0.15, 5, 8]} />
-        <meshStandardMaterial
-          color="#ded3c5"
-          roughness={0.9}
-        />
-      </mesh>
-
-      {/* Window frame */}
-      <mesh position={[0, 2.6, -3.9]}>
-        <boxGeometry args={[3.8, 2.4, 0.08]} />
-        <meshStandardMaterial
-          color="#3b3028"
-          roughness={0.7}
-        />
-      </mesh>
-
-      {/* Window glass */}
-      <mesh position={[0, 2.6, -3.84]}>
-        <planeGeometry args={[3.45, 2.05]} />
-        <meshStandardMaterial
-          color="#9db7c5"
-          roughness={0.25}
-          metalness={0.05}
-        />
-      </mesh>
-
-      {/* Window vertical divider */}
-      <mesh position={[0, 2.6, -3.75]}>
-        <boxGeometry args={[0.08, 2.1, 0.1]} />
-        <meshStandardMaterial color="#3b3028" />
-      </mesh>
-
-      {/* Window horizontal divider */}
-      <mesh position={[0, 2.6, -3.75]}>
-        <boxGeometry args={[3.5, 0.08, 0.1]} />
-        <meshStandardMaterial color="#3b3028" />
-      </mesh>
-
-      {/* Plant pot */}
-      <mesh position={[-4, 0.55, -2.5]}>
-        <cylinderGeometry args={[0.35, 0.28, 0.65, 32]} />
-        <meshStandardMaterial
-          color="#8b5a3c"
-          roughness={0.9}
-        />
-      </mesh>
-
-      {/* Plant stem */}
-      <mesh position={[-4, 1.4, -2.5]}>
-        <cylinderGeometry args={[0.045, 0.045, 1.6, 12]} />
-        <meshStandardMaterial color="#405438" />
-      </mesh>
-
-      {/* Plant leaves */}
-      <mesh position={[-3.65, 1.65, -2.5]} rotation={[0, 0, -0.45]}>
-        <sphereGeometry args={[0.28, 16, 16]} />
-        <meshStandardMaterial color="#526b42" roughness={0.9} />
-      </mesh>
-
-      <mesh position={[-4.3, 1.8, -2.5]} rotation={[0, 0, 0.45]}>
-        <sphereGeometry args={[0.3, 16, 16]} />
-        <meshStandardMaterial color="#526b42" roughness={0.9} />
-      </mesh>
-
-      <mesh position={[-4, 2.05, -2.5]}>
-        <sphereGeometry args={[0.3, 16, 16]} />
-        <meshStandardMaterial color="#60794b" roughness={0.9} />
-      </mesh>
-
-      {/* Coffee cup */}
-      <group position={[1.65, 0.98, -0.75]}>
+      {/* =========================
+          WINDOW
+      ========================= */}
+      <group position={[0, 3.1, -3.88]}>
+        {/* Glass */}
         <mesh>
-          <cylinderGeometry args={[0.28, 0.23, 0.45, 32]} />
+          <planeGeometry args={[4.5, 2.6]} />
           <meshStandardMaterial
-            color="#f1eee8"
-            roughness={0.6}
+            color="#9bb9c7"
+            roughness={0.25}
+            metalness={0.05}
+          />
+        </mesh>
+
+        {/* Outer frame */}
+        <mesh position={[0, 1.35, 0.05]}>
+          <boxGeometry args={[4.8, 0.12, 0.12]} />
+          <meshStandardMaterial color="#3b2b21" />
+        </mesh>
+
+        <mesh position={[0, -1.35, 0.05]}>
+          <boxGeometry args={[4.8, 0.12, 0.12]} />
+          <meshStandardMaterial color="#3b2b21" />
+        </mesh>
+
+        <mesh position={[-2.35, 0, 0.05]}>
+          <boxGeometry args={[0.12, 2.8, 0.12]} />
+          <meshStandardMaterial color="#3b2b21" />
+        </mesh>
+
+        <mesh position={[2.35, 0, 0.05]}>
+          <boxGeometry args={[0.12, 2.8, 0.12]} />
+          <meshStandardMaterial color="#3b2b21" />
+        </mesh>
+
+        {/* Window cross */}
+        <mesh position={[0, 0, 0.06]}>
+          <boxGeometry args={[0.08, 2.6, 0.1]} />
+          <meshStandardMaterial color="#3b2b21" />
+        </mesh>
+
+        <mesh position={[0, 0, 0.06]}>
+          <boxGeometry args={[4.5, 0.08, 0.1]} />
+          <meshStandardMaterial color="#3b2b21" />
+        </mesh>
+      </group>
+
+      {/* =========================
+          CHAIR
+      ========================= */}
+      <group position={[3.0, 0, 1.7]}>
+        {/* Seat */}
+        <mesh position={[0, 0.72, 0]}>
+          <boxGeometry args={[1.35, 0.16, 1.25]} />
+          <meshStandardMaterial
+            color="#71482f"
+            roughness={0.85}
+          />
+        </mesh>
+
+        {/* Back */}
+        <mesh position={[0, 1.65, 0.5]}>
+          <boxGeometry args={[1.35, 1.8, 0.16]} />
+          <meshStandardMaterial
+            color="#71482f"
+            roughness={0.85}
+          />
+        </mesh>
+
+        {/* Legs */}
+        <mesh position={[-0.5, 0.35, -0.42]}>
+          <cylinderGeometry args={[0.06, 0.06, 0.7, 12]} />
+          <meshStandardMaterial color="#4b3021" />
+        </mesh>
+
+        <mesh position={[0.5, 0.35, -0.42]}>
+          <cylinderGeometry args={[0.06, 0.06, 0.7, 12]} />
+          <meshStandardMaterial color="#4b3021" />
+        </mesh>
+
+        <mesh position={[-0.5, 0.35, 0.42]}>
+          <cylinderGeometry args={[0.06, 0.06, 0.7, 12]} />
+          <meshStandardMaterial color="#4b3021" />
+        </mesh>
+
+        <mesh position={[0.5, 0.35, 0.42]}>
+          <cylinderGeometry args={[0.06, 0.06, 0.7, 12]} />
+          <meshStandardMaterial color="#4b3021" />
+        </mesh>
+      </group>
+
+      {/* =========================
+          COFFEE CUP
+      ========================= */}
+      <group position={[1.35, 0.92, -0.7]}>
+        {/* Cup */}
+        <mesh>
+          <cylinderGeometry args={[0.25, 0.21, 0.42, 32]} />
+          <meshStandardMaterial
+            color="#f4f0e8"
+            roughness={0.55}
           />
         </mesh>
 
         {/* Coffee */}
-        <mesh position={[0, 0.23, 0]}>
-          <cylinderGeometry args={[0.22, 0.22, 0.015, 32]} />
+        <mesh position={[0, 0.215, 0]}>
+          <cylinderGeometry args={[0.20, 0.20, 0.018, 32]} />
           <meshStandardMaterial
-            color="#3b2417"
-            roughness={0.5}
+            color="#3b2114"
+            roughness={0.4}
           />
         </mesh>
 
         {/* Handle */}
         <mesh
-          position={[0.31, 0, 0]}
+          position={[0.28, 0, 0]}
           rotation={[Math.PI / 2, 0, 0]}
         >
-          <torusGeometry args={[0.13, 0.045, 12, 24]} />
+          <torusGeometry args={[0.12, 0.04, 12, 24]} />
           <meshStandardMaterial
-            color="#f1eee8"
+            color="#f4f0e8"
+            roughness={0.55}
+          />
+        </mesh>
+
+        {/* Saucer */}
+        <mesh position={[0, -0.22, 0]}>
+          <cylinderGeometry args={[0.38, 0.38, 0.035, 32]} />
+          <meshStandardMaterial
+            color="#eee9df"
             roughness={0.6}
           />
         </mesh>
       </group>
 
-      {/* Chair */}
-      <group position={[3.2, 0.65, 1.8]}>
-        {/* Seat */}
-        <mesh position={[0, 0, 0]}>
-          <boxGeometry args={[1.4, 0.15, 1.3]} />
+      {/* =========================
+          PLANT
+      ========================= */}
+      <group position={[-3.5, 0, -2.5]}>
+        {/* Pot */}
+        <mesh position={[0, 0.45, 0]}>
+          <cylinderGeometry args={[0.38, 0.30, 0.7, 32]} />
           <meshStandardMaterial
-            color="#6f4932"
+            color="#9a6344"
             roughness={0.9}
           />
         </mesh>
 
-        {/* Back */}
-        <mesh position={[0, 0.9, 0.55]}>
-          <boxGeometry args={[1.4, 1.7, 0.15]} />
+        {/* Stem */}
+        <mesh position={[0, 1.45, 0]}>
+          <cylinderGeometry args={[0.045, 0.045, 1.8, 12]} />
+          <meshStandardMaterial color="#405336" />
+        </mesh>
+
+        {/* Leaves */}
+        <mesh
+          position={[-0.35, 1.75, 0]}
+          rotation={[0, 0, -0.55]}
+        >
+          <sphereGeometry args={[0.32, 20, 20]} />
           <meshStandardMaterial
-            color="#6f4932"
+            color="#506b43"
             roughness={0.9}
           />
         </mesh>
 
-        {/* Legs */}
-        {[
-          [-0.5, -0.55],
-          [0.5, -0.55],
-          [-0.5, 0.45],
-          [0.5, 0.45],
-        ].map(([x, z], i) => (
-          <mesh
-            key={i}
-            position={[x, -0.55, z]}
-          >
-            <cylinderGeometry args={[0.07, 0.07, 1.1, 12]} />
-            <meshStandardMaterial
-              color="#4a3021"
-              roughness={0.9}
-            />
-          </mesh>
-        ))}
+        <mesh
+          position={[0.35, 1.95, 0]}
+          rotation={[0, 0, 0.55]}
+        >
+          <sphereGeometry args={[0.32, 20, 20]} />
+          <meshStandardMaterial
+            color="#587449"
+            roughness={0.9}
+          />
+        </mesh>
+
+        <mesh
+          position={[0, 2.25, 0]}
+        >
+          <sphereGeometry args={[0.34, 20, 20]} />
+          <meshStandardMaterial
+            color="#607b4e"
+            roughness={0.9}
+          />
+        </mesh>
+
+        <mesh
+          position={[-0.4, 2.15, 0.15]}
+        >
+          <sphereGeometry args={[0.28, 20, 20]} />
+          <meshStandardMaterial
+            color="#4d6840"
+            roughness={0.9}
+          />
+        </mesh>
       </group>
     </group>
   );
