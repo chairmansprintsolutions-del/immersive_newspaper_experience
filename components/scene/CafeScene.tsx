44eb15bd5c48e04d23bdb28cb607af4fbe93ca83
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-08-22 11:19:35 +0530

    Update CafeScene.tsx

--- components/scene/CafeScene.tsx
+++ components/scene/CafeScene.tsx
@@ -12,14 +12,8 @@ export default function CafeScene() {
       />
 
       <Model
-        path="/model/table.glb"
-        position={[0, 0, 0]}
-        scale={1}
-      />
-
-      <Model
         path="/model/coffee_cup.glb"
-        position={[1.6, 0.05, -0.8]}
+        position={[1.6, 0.85, -0.8]}
         scale={0.4}
       />
 
