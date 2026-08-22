b76f4cc346f4ab43873ac4fc6015898c7e5713f8
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-08-22 13:01:31 +0530

    Update Experience.tsx

--- components/scene/Experience.tsx
+++ components/scene/Experience.tsx
@@ -3,6 +3,7 @@
 import { OrbitControls } from "@react-three/drei";
 import Floor from "./Floor";
 import Table from "./Table";
+import NewspaperScene from "../newspaper/NewspaperScene";
 
 export default function Experience() {
   return (
@@ -20,10 +21,20 @@ export default function Experience() {
       <Floor />
       <Table />
 
+      <group
+        position={[0, 0.82, 0]}
+        rotation={[0, 0, 0]}
+        scale={[0.7, 0.7, 0.7]}
+      >
+        <NewspaperScene />
+      </group>
+
       <OrbitControls
         makeDefault
         enablePan={false}
         target={[0, 0.75, 0]}
+        minDistance={3}
+        maxDistance={8}
       />
     </>
   );
