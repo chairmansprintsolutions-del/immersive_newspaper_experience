6a75439bb3c50cfd2d2d190b63dae38ea7c0fa72
chairmansprintsolutions-del <chairman.sprintsolutions@gmail.com>
2026-08-22 13:07:57 +0530

    Update Experience.tsx

--- components/scene/Experience.tsx
+++ components/scene/Experience.tsx
@@ -4,6 +4,8 @@ import { OrbitControls } from "@react-three/drei";
 
 import Floor from "./Floor";
 import Table from "./Table";
+import Lights from "./Lights";
+import CafeScene from "./CafeScene";
 import NewspaperScene from "../newspaper/NewspaperScene";
 
 export default function Experience() {
@@ -11,22 +13,14 @@ export default function Experience() {
     <>
       <color attach="background" args={["#f6f2eb"]} />
 
-      <ambientLight intensity={1.5} />
-
-      <directionalLight
-        castShadow
-        position={[4, 8, 5]}
-        intensity={2}
-      />
+      <Lights />
 
       <Floor />
       <Table />
 
-      {/* Newspaper sitting directly on the table */}
-      <group
-        position={[0, 0.795, 0]}
-        scale={[0.7, 0.7, 0.7]}
-      >
+      <CafeScene />
+
+      <group position={[0, 0.795, 0]}>
         <NewspaperScene />
       </group>
 
@@ -35,7 +29,7 @@ export default function Experience() {
         enablePan={false}
         minDistance={3}
         maxDistance={8}
-        target={[0, 0.75, 0]}
+        maxPolarAngle={Math.PI / 2.05}
       />
     </>
   );
